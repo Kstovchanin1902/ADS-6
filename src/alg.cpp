539b685ca30edb783d749c542708357d27909e45
Kstovchanin1902 <78904695+Kstovchanin1902@users.noreply.github.com>
2021-05-24 20:49:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,40 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
-
+void downReg(std::string &str) {
+    char* up =  (char*) str.c_str();
+    std::string tranz = "";
+    int count = str.length(); 
+    for (int i = 0; i < count; i++) {
+        if (up[i] >= 'A' && up[i] <= 'Z')
+            up[i] += ('a' - 'A');
+        tranz.push_back(up[i]);
+    }
+    str = tranz;
+}
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+std::ifstream fin (filename);
+    BST<std::string> wAP;
+    if (fin.is_open()) {
+        std::string outCh;
+        char symb = ' ';
+        std::string res;
+        while (!fin.eof()) {
+           getline(fin, outCh);
+           for (int i = 0; i < outCh.length(); i++)
+               if ((outCh[i] >= 'A' && outCh[i] <= 'Z') ||
+               (outCh[i] >= 'a' && outCh[i] <= 'z')) {
+                   symb = outCh[i];
+                   res.push_back(symb);
+               }
+               else {
+                if (res != "") {
+                downReg(res);
+                wAP.add(res);
+                }
+                res = "";
+               }
+        }
+    }
+    return wAP;
 }
